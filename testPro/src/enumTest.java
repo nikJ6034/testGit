b6930e9cfa8dda041253d4df37525453b842a490
nikJ6034 <jmi7313@naver.com>
2016-03-04 10:32:33 +0900

    Update enumTest.java

--- testPro/src/enumTest.java
+++ testPro/src/enumTest.java
@@ -4,7 +4,7 @@ public class enumTest {
 	
 	public enum Season{
 		
-		SPRING("March through May"),
+		SPRING("March through May321"),
 		SUMMER("June through August"),
 		FALL("September through November"),
 		WINTER("December through February");
